7c34169dbf3755f6d25e1b8fa41b90ebed373acf
HoomanJ <Hooman.Jalalpoor@Gmail.com>
2026-05-30 18:14:36 +0330

    config update

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 import os
+from typing import List
 from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-BASE_DOWNLOAD_LINK = os.getenv("BASE_DOWNLOAD_LINK", "http://localhost:8000")
-WHITELIST_USERS = [int(u) for u in os.getenv("WHITELIST_USERS", "").split(",") if u]
-STORAGE_DAYS = int(os.getenv("STORAGE_DAYS", "2"))
-MAX_TELEGRAM_FILE_SIZE = int(os.getenv("MAX_TELEGRAM_FILE_SIZE", "52428800"))
-
-DOWNLOAD_DIR = "downloads"
-COOKIE_DIR = "data/cookies"
-USER_COOKIES_FILE = "data/user_cookies.json"
-USER_VIDEOS_FILE = "data/user_videos.json"
+class Config:
+    def __init__(self):
+        self.BOT_TOKEN = os.getenv('BOT_TOKEN', '')
+        self.BASE_DOWNLOAD_LINK = os.getenv('BASE_DOWNLOAD_LINK', 'http://localhost:8000')
+        self._whitelist = os.getenv('WHITELIST_USERS', '')
+        self.STORAGE_DAYS = int(os.getenv('STORAGE_DAYS', '2'))
+        self.MAX_TELEGRAM_FILE_SIZE = int(os.getenv('MAX_TELEGRAM_FILE_SIZE', '50')) * 1024 * 1024
+    
+    def get_whitelist(self) -> List[int]:
+        return [int(u) for u in self._whitelist.split(',') if u] if self._whitelist else []
